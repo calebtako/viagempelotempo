53ad989b9792f257957db0e5bbb72ea5db9dd592
calebtako <calebtako@gmail.com>
2018-04-05 19:36:27 -0300

    g1

---
+++ ViagemPeloTempo/ViagemPeloTempo/Controllers/PagSelecaoController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ViagemPeloTempo.Models;
+namespace ViagemPeloTempo.Controllers
+{
+    public class PagSelecaoController : Controller
+    {
+        // GET: PagSelecao
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
