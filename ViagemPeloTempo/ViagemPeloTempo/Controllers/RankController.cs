0dadf8ff0d5f3dd3e493b73a330b803a3f50718b
calebtako <calebtako@gmail.com>
2018-04-11 20:05:22 -0300

    I1
    kkk

---
+++ ViagemPeloTempo/ViagemPeloTempo/Controllers/RankController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ViagemPeloTempo.Controllers
+{
+    public class RankController : Controller
+    {
+        // GET: Rank
+        public ActionResult RankJ()
+        {
+            return View();
+        }
+    }
+}
