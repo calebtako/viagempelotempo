4436915c52a8b0ddc06f01e7adf76994e54a2399
calebtako <calebtako@gmail.com>
2018-04-17 21:43:13 -0300

    K1

---
+++ ViagemPeloTempo/ViagemPeloTempo/Controllers/JogosController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ViagemPeloTempo.Controllers
+{
+    public class JogosController : Controller
+    {
+        // GET: Jogos
+        public ActionResult PagJogo()
+        {
+            return View();
+        }
+    }
+}
