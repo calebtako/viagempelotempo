9031a69eed8509b75bacda6c73b87c9ef7393d8b
calebtako <calebtako@gmail.com>
2018-04-18 22:18:48 -0300

    L7

--- ViagemPeloTempo/ViagemPeloTempo.DataAccess/PerguntaDAO.cs
+++ ViagemPeloTempo/ViagemPeloTempo.DataAccess/PerguntaDAO.cs
@@ -3,10 +3,58 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data.Sql;
+using System.Data;
+using ViagemPeloTempo.Models;
 
 namespace ViagemPeloTempo.DataAccess
 {
-    class Perguntas
+    public class PerguntaDAO
     {
+        public Questao Buscar(int IdQuest)
+        {
+            //Criando uma conexão com o banco de dados
+            using (SqlConnection conn =
+                new SqlConnection(@"Initial Catalog=viagempelotempo;
+                                    Data Source=localhost;
+                                    Integrated Security=SSPI;"))
+            {
+                //Criando instrução sql para selecionar todos os registros na tabela de contatos
+                string strSQL = @"SELECT q.texto, a.texto FROM questao as q where id = " + IdQuest + " inner join alternativa as a on q.idquest = a.idquest;";
+
+                //Criando um comando sql que será executado na base de dados
+                using (SqlCommand cmd = new SqlCommand(strSQL))
+                {
+                    //Abrindo conexão com o banco de dados
+                    conn.Open();
+                    cmd.Connection = conn;
+                    cmd.CommandText = strSQL;
+                    //Executando instrução sql
+                    var dataReader = cmd.ExecuteReader();
+                    var dt = new DataTable();
+                    dt.Load(dataReader);
+                    //Fechando conexão com o banco de dados
+                    conn.Close();
+
+                    if (dt == null || dt.Rows.Count <= 0)
+                        return null;
+
+                    var row = dt.Rows[0];
+
+                    var questao = new Questao()
+                    {
+                        IdQuestao = Convert.ToInt32(row["id"]),
+                        Codigo = row["codigo"].ToString(),
+                        Cliente = row["cliente"].ToString(),
+                        Data = Convert.ToDateTime(row["data"])
+                    };
+
+                    return questao;
+                }
+            }
+
+        }
+
     }
 }
